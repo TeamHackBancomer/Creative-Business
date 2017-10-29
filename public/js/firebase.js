cca87a0d5cc68140cc3b372f1e24a284982702b2
David Moranchel <david.arcshtag@gmail.com>
2017-10-28 22:23:04 -0500

    :octocat:

---
+++ public/js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyDiWIivdO83bNC7OwicWC8N7NMq9CpoOYI",
+  authDomain: "creative-business-hb.firebaseapp.com",
+  databaseURL: "https://creative-business-hb.firebaseio.com",
+  projectId: "creative-business-hb",
+  storageBucket: "creative-business-hb.appspot.com",
+  messagingSenderId: "487883854645"
+};
+firebase.initializeApp(config);
