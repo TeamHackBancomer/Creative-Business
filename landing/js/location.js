04138e0160dbb6f1f1491ba6ea8f374a65b14fb7
Aarón Cervantes <metallicaaron@hotmail.es>
2017-10-29 00:41:01 -0500

    Added new form

---
+++ landing/js/location.js
@@ -0,0 +1,13 @@
+window.onload = getLocation();
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+function showPosition(position) {
+    var position = position.coords.latitude +", "+ position.coords.longitude;
+}
+
