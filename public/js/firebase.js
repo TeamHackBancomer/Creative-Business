d1948e89be7223f24a97baa0f772b8dbc7277a7b
David Moranchel <david.arcshtag@gmail.com>
2017-10-29 13:35:58 -0600

    preloader form

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -29,33 +29,64 @@ function writeUserData(name, sex, age, monthly_income, credit_card, debit_card, 
     window.location.href = 'thank.html';
   });
 }
-
+var myDoughnut, myDoughnutBusiness;
 window.onload = function() {
 
-  window.myDoughnut = new Chart(ctx, config);
+   var canvas = document.getElementById("pie-chart");
+       canvas.height = 100;
+   var canvasB = document.getElementById("business");
+       canvasB.height = 100;
+   var ctx = document.getElementById("pie-chart").getContext("2d");
+   var ctxB = document.getElementById("business").getContext("2d");
+   window.myDoughnut = new Chart(ctx, config);
+   window.myDoughnutBusiness = new Chart(ctxB, configB);
 }
-var canvas = document.getElementById("pie-chart");
-    canvas.height =50;
-var ctx = document.getElementById("pie-chart").getContext("2d");
+
 
 ref_user.on('child_added',function (snapshot) {
-    if (snapshot.val().credit_card == 1 && snapshot.val().debit_card == 1) {
-      config['data'].datasets[0]['data'][2] += 1;
-    } else if ( snapshot.val().credit_card == 0 && snapshot.val().debit_card == 0 ) {
-      config['data'].datasets[0]['data'][3] += 1;
-    } else if ( snapshot.val().credit_card == 1 && snapshot.val().debit_card == 0 ) {
-      config['data'].datasets[0]['data'][0] += 1;
-    } else if ( snapshot.val().credit_card == 0 && snapshot.val().debit_card == 1 ) {
-      config['data'].datasets[0]['data'][1] += 1;
-    }
-    window.myDoughnut.update();
+    cards(snapshot);
+    business(snapshot);
+    myDoughnut.update();
+    myDoughnutBusiness.update();
 });
 
+function cards(snapshot) {
+  if (snapshot.val().credit_card == 1 && snapshot.val().debit_card == 1) {
+    config['data'].datasets[0]['data'][2] += 1;
+  } else if ( snapshot.val().credit_card == 0 && snapshot.val().debit_card == 0 ) {
+    config['data'].datasets[0]['data'][3] += 1;
+  } else if ( snapshot.val().credit_card == 1 && snapshot.val().debit_card == 0 ) {
+    config['data'].datasets[0]['data'][0] += 1;
+  } else if ( snapshot.val().credit_card == 0 && snapshot.val().debit_card == 1 ) {
+    config['data'].datasets[0]['data'][1] += 1;
+  }
+}
 
-
+function business(snapshot) {
+  console.log(snapshot.val().business);
+  if (snapshot.val().business == 'Supermercado') {
+    configB['data'].datasets[0]['data'][0] += 1;
+  }else if (snapshot.val().business == 'Farmacia') {
+    configB['data'].datasets[0]['data'][1] += 1;
+  }else if (snapshot.val().business == 'Tiendas de Electronicos') {
+    configB['data'].datasets[0]['data'][2] += 1;
+  }else if (snapshot.val().business == 'Cine') {
+    configB['data'].datasets[0]['data'][3] += 1;
+  }else if (snapshot.val().business == 'Médico') {
+    configB['data'].datasets[0]['data'][4] += 1;
+  }else if (snapshot.val().business == 'Restaurantes') {
+    configB['data'].datasets[0]['data'][5] += 1;
+  }else if (snapshot.val().business == 'Banco') {
+    configB['data'].datasets[0]['data'][6] += 1;
+  }else if (snapshot.val().business == 'Expendedora') {
+    configB['data'].datasets[0]['data'][7] += 1;
+  }else if (snapshot.val().business == 'Bares') {
+    configB['data'].datasets[0]['data'][8] += 1;
+  }
+}
 
 var config = {
-    type: 'pie',
+    type: 'doughnut',
     data: {
         labels: ["Crédito", "Débito","Ambas [Crédito/Débito]","Ninguna"],
         datasets: [{
@@ -89,6 +120,44 @@ var config = {
         }
     }
 };
-function update() {
-    window.myDoughnut.update();
-}
+
+var configB = {
+    type: 'polarArea',
+    data: {
+        labels: ["Supermercado", "Farmacia","Electronicos","Cine","Médico","Restaurantes","Banco","Expendedora","Bares"],
+        datasets: [{
+            label: 'Tarjetas',
+            data: [0,0,0,0,0,0,0,0,0],
+            backgroundColor: [
+                'rgba(25, 118, 210, 1)',
+                'rgba(245, 124, 0, 1)',
+                'rgba(104, 159, 56, 1)',
+                'rgba(194, 24, 91, 1)',
+                'rgba(192, 21, 31, 1)',
+                'rgba(25, 128, 110, 1)',
+                'rgba(241, 124, 3, 1)',
+                'rgba(134, 159, 56, 1)',
+                'rgba(144, 14, 91, 1)'
+            ],
+            // borderColor: [
+            //     'rgba(13, 71, 161, 1)',
+            //     'rgba(230, 81, 0, 1)',
+            //     'rgba(51, 105, 30, 1)',
+            //     'rgba(136, 14, 79, 1)'
+            // ],
+            borderWidth: 2,
+            hoverBackgroundColor: [
+                'rgba(33, 150, 243, 1)',
+                'rgba(255, 152, 0, 1)',
+                'rgba(139, 195, 74, 1)',
+                'rgba(233, 30, 99, 1)'
+            ]
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Tarjetas con respecto a los usuarios'
+        }
+    }
+};
