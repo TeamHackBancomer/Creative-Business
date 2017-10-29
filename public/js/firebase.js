016044d2785628239f3b7be82d42476b6751a08b
David Moranchel <david.arcshtag@gmail.com>
2017-10-29 07:49:51 -0600

    formulario conectado

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -8,12 +8,41 @@ var config = {
 };
 firebase.initializeApp(config);
 //database
-const db = firebase.database();
-const ref_user = db.ref('user');
+var database = firebase.database();
+var ref_user = database.ref('user');
+var latitude, longitude;
 
-function writeUserData(name, sex, age, monthly_income, credit_card, debit_card, lat, lng, shops) {
+function submit() {
+  name = $('#first_name').val();
+  sex  = $('#sex').val();
+  age  = $('#age').val();
+  if ( $('#card').val() == 1 ) {
+    credit_card = 1;
+    debit_card  = 0;
+  } else if ( $('#card').val() == 2 ) {
+    credit_card = 0;
+    debit_card  = 1;
+  } else if ( $('#card').val() == 3 ) {
+    credit_card = 1;
+    debit_card  = 1;
+  } else if ( $('#card').val() == 4 ) {
+    credit_card = 0;
+    debit_card  = 0;
+  }
+  monthly_income = $('#ingreso').val();
+  business = $('#business').val();
+  lat = latitude;
+  lng = longitude;
+  writeUserData(name, sex, age, monthly_income, credit_card, debit_card, lat, lng, business)
+}
+
+$('#submit_button').on('click', function () {
+  submit();
+});
+
+function writeUserData(name, sex, age, monthly_income, credit_card, debit_card, lat, lng, business) {
   ref_user.push({
-    username:       name,
+    name:           name,
     sex:            sex,
     age:            age,
     monthly_income: monthly_income,
@@ -22,5 +51,22 @@ function writeUserData(name, sex, age, monthly_income, credit_card, debit_card, 
     lat:            lat,
     lng:            lng,
     shops:          shops
+  }).then(function (message) {
+    console.log(message,'message');
   });
+}
+
+
+window.onload = getLocation();
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+function showPosition(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 }
--- public/js/location.js
+++ public/js/location.js
@@ -1,13 +1,12 @@
-window.onload = getLocation();
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    }
-}
-function showPosition(position) {
-    var position = position.coords.latitude +", "+ position.coords.longitude;
-}
-
+// window.onload = getLocation();
+//
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     }
+// }
+// function showPosition(position) {
+//     var position = position.coords.latitude +", "+ position.coords.longitude;
+// }
